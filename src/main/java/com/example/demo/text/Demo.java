8b28cd7a12c3ae009cfcf519d0f4ba47176a2842
侯爵 <749899757@qq.com>
2021-06-23 14:40:28 +0800

    commit sout2

--- src/main/java/com/example/demo/text/Demo.java
+++ src/main/java/com/example/demo/text/Demo.java
@@ -10,5 +10,6 @@ public class Demo {
     public static void main(String[] args) throws ParseException {
         String content =  String.format("message", "1a2e");
         System.out.println(content);
+        System.out.println(content);
     }
 }
