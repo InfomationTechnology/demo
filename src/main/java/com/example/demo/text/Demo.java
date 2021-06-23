344fc7bcdd9083826f4d01f303a3d2cae52e6232
侯爵 <749899757@qq.com>
2021-06-23 11:54:13 +0800

    commit directory

---
+++ src/main/java/com/example/demo/text/Demo.java
@@ -0,0 +1,14 @@
+package com.example.demo.text;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Demo {
+
+    public static void main(String[] args) throws ParseException {
+        String content =  String.format("message", "1a2e");
+        System.out.println(content);
+    }
+}
