b23ffbf3034e5ca74abc261906dc42b80439929d
侯爵 <749899757@qq.com>
2021-06-29 16:33:49 +0800

    commit sout2

---
+++ src/main/java/com/example/demo/text/Demo2.java
@@ -0,0 +1,13 @@
+package com.example.demo.text;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Demo2 {
+    private String name;
+    private boolean abc;
+    private int age;
+
+}
