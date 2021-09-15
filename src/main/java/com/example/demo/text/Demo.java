65da9a00019e3a408faa11fec476e47fee13bd34
侯爵 <749899757@qq.com>
2021-09-15 10:40:26 +0800

    commit sout2

--- src/main/java/com/example/demo/text/Demo.java
+++ src/main/java/com/example/demo/text/Demo.java
@@ -1,22 +1,77 @@
 package com.example.demo.text;
 
+import com.example.demo.text.model.User;
+
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Demo {
 
     public static void main(String[] args) throws ParseException {
-        String content =  String.format("message", "1a2e3");
-        System.out.println(content);
-        System.out.println("this is test");
-        System.out.println(content);
-        Map map = new HashMap();
-        map.put("page","8");
-        map.put("limit","10");
-        System.out.println(map);
+        //String content =  String.format("message", "1a2e3");
+        //System.out.println(content);
+        //System.out.println("this is test");
+        //System.out.println(content);
+        //Map map = new HashMap();
+        //map.put("page","8");
+        //map.put("limit","10");
+        //System.out.println(map);
+
+        List<User> listUser1 = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            User user = new User();
+            if(i==0){user.setId(598);}else if(i==1){user.setId(324);}else if(i==2){user.setId(517);}else if(i==3){user.setId(976);}
+            else if(i==4){user.setId(653);}else if(i==5){user.setId(178);}else if(i==6){user.setId(459);}
+            user.setName("鲁班"+i+"号");
+            user.setAge(i+10);
+            listUser1.add(user);
+        }
+
+        List<User> listUser2 = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            User user = new User();
+            if(i==0){user.setId(976);}else if(i==1){user.setId(178);}else if(i==2){user.setId(653);}else if(i==3){user.setId(598);}
+            user.setName("妲己"+i+"号");
+            user.setAge(i+26);
+            listUser2.add(user);
+        }
+
+
+        StringJoiner sj1 = new StringJoiner(",");
+        listUser1.forEach(l1->sj1.add(l1.getId()+""));
+
+        StringJoiner sj2 = new StringJoiner(",");
+        listUser2.forEach(l2->sj2.add(l2.getId()+""));
+
+        System.out.println("listUser1:"+sj1);
+        System.out.println("listUser2:"+sj2);
+
+        Map<Long,User> map = new HashMap();
+        listUser2.forEach(l2->map.put(l2.getId(),l2));
+        List<User> list3 = new ArrayList<>();
+        listUser1.forEach(l1->{
+            if(map.containsKey(l1.getId())){
+                list3.add(map.get(l1.getId()));
+            }
+        });
+        list3.forEach(System.out::println);
+
+
+
+        /*Map<String,Object> map = new HashMap();
+        List<String> list1 = Arrays.asList("1", "2", "3", "4", "5");
+        List<String> list2 = Arrays.asList("3", "5", "4");
+        list2.forEach(l2->map.put(l2,l2));
+        List<String> list3 = new ArrayList<>();
+        list1.forEach(l1->{
+            if(map.containsKey(l1)){
+                list3.add(map.get(l1).toString());
+            }
+        });
+        list3.forEach(System.out::println);*/
+
+
     }
 }
--- src/main/java/com/example/demo/text/Demo2.java
+++ src/main/java/com/example/demo/text/Demo2.java
@@ -3,6 +3,11 @@ package com.example.demo.text;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @Data
 @NoArgsConstructor
 public class Demo2 {
@@ -10,7 +15,112 @@ public class Demo2 {
     private boolean abc;
     private int age2;
 
+    //public static void main(String[] args) {
+    //    /*System.out.println("Hello World!");
+    //
+    //    // 顺序标准
+    //    ArrayList<Integer> base = new ArrayList<>(Arrays.asList(5, 4, 6, 1, 3, 2));
+    //
+    //    ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+    //
+    //    // 取出顺序
+    //    Map<Integer, Integer> cache = new HashMap<>();
+    //    AtomicInteger adder = new AtomicInteger();
+    //    base.forEach(item -> cache.put(item, adder.getAndIncrement()));
+    //
+    //    // 按 base 排序
+    //    list.sort((i1, i2) -> {
+    //        return cache.get(i1) - cache.get(i2);
+    //    });
+    //
+    //    System.out.println(list);*/
+    //
+    //    StringBuilder sb = new StringBuilder();
+    //    IntStream.range(1,10).forEach(i->{
+    //        sb.append(i+"");
+    //        if( i < 9){
+    //            sb.append(",");
+    //        }
+    //    });
+    //    System.out.println(sb.toString());
+    //
+    //    StringJoiner sj = new StringJoiner(",");
+    //    IntStream.range(1,10).forEach(i->sj.add(i+""));
+    //    System.out.println(sj.toString());
+    //}
+
     public static void main(String[] args) {
+        trim(" i   am   a   good    man ");
+    }
+    public static void trim(String str){
+        System.out.println("输入："+str);
+        char[] aa = str.toCharArray();
+
+        str = str.trim();
+        String reverse = "";
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            reverse = str.charAt(i) + reverse;
+        }
+        System.out.println("反转："+reverse);
+
+        reverse = str;
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        char[] bb = reverse.toCharArray();
+
+        for (int i = 0; i < bb.length; i++) {
+            char c = bb[i];
+            if(c==' '){
+                int bbsize = bb.length;
+                stringBuilder.append(" ");
+                for (int j = i+1; j <= bbsize; j++) {
+                    if(bb[j]!=' '){
+                        stringBuilder.append(bb[j]);
+                        i=j;
+                        bbsize=j;
+                        break;
+                    }
+
+                }
+            }else {
+                stringBuilder.append(c);
+            }
+        }
+        //System.out.println("输出："+stringBuilder.toString());
+        StringJoiner sj1 = new StringJoiner(" ");
+        String[] split = stringBuilder.toString().split(" ");
+        for (int i = 0; i < split.length / 2; i++) {
+            //进行数组互换
+            String sum = split[i];
+            split[i] = split[split.length - 1 - i];
+            split[split.length - 1 - i] = sum;
+        }
+
+        for (int i = 0; i < split.length; i++) {
+            sj1.add(split[i]);
+        }
+
+        System.out.println("输出："+sj1);
+
+        System.out.println("=================================================");
+
+
+        String str1 = " i am    a     good man  ";
+        System.out.println("str1 = " + str1);
+
+        String trimStr = str1.trim().replaceAll("\\s+", " ");
+        System.out.println("trimStr = " + trimStr);
+
+        List<String> words = Arrays.asList(trimStr.split(" "));
+
+        Collections.reverse(words);
+
+        String res = words.stream().collect(Collectors.joining(" "));
+
+        System.out.println("res = " + res);
+
 
     }
 
